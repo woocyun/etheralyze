8835d74d132a3bc68eab758666ffab07ff9dad03
Woo Yun <woocyun@gmail.com>
2017-11-21 19:18:14 -0800

    add pagination response to blocks request

--- server/controllers/block.controller.js
+++ server/controllers/block.controller.js
@@ -4,13 +4,42 @@ const Block = require('../models/block.model');
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA));
 
 function getBlocks(req, res) {
-  Block
-    .find({})
-    .sort({ number: -1 })
-    .limit(50)
-    .exec((err, blocks) => {
-      if (err) res.status(500).send(err);
-      res.send(blocks);
+  const promises = [];
+
+  promises.push(
+    new Promise((resolve, reject) => {
+      Block.count()
+        .exec((err, count) => {
+          if (err) reject(err);
+          resolve(count);
+        });
+    }),
+    new Promise((resolve, reject) => {
+      Block
+        .find({})
+        .sort({ number: -1 })
+        .limit(50)
+        .exec((err, blocks) => {
+          if (err) reject(err);
+          resolve(blocks);
+        });
+    })
+  );
+
+  Promise.all(promises)
+    .then(response => {
+      const totalBlocks = response[0];
+      const blocks = response[1];
+
+      res.send({
+        pagination: {
+          total: totalBlocks
+        },
+        blocks
+      });
+    })
+    .catch(err => {
+      res.status(400).send(err);
     });
 }
 
