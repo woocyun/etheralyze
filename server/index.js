c2d8e533d13213d4e82a42a64e9363dd28aafa01
Woo Yun <woocyun@gmail.com>
2017-11-27 21:24:00 -0800

    serve static files from /assets

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.set('port', process.env.PORT || 9000);
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
 
-app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use('/assets', express.static(path.resolve(__dirname, '..', 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
