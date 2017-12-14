b6aad1f9ce9a39f8f8970aeea32c64f089307ffe
Woo Yun <woocyun@gmail.com>
2017-12-14 00:56:30 -0800

    mobile nav toggle, styling, more menu items

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Link
 } from 'react-router-dom';
@@ -6,30 +6,89 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui-icons/MoreVert';
 
-const Header = () => {
-  return (
-    <div className="header">
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography type="headline" className="title">
-            Etheralyze
-          </Typography>
-          <nav className="desktop-nav">
-            <Link to="/blocks">
-              <Button color="accent">Blocks</Button>
-            </Link>
-            <Link to="/transactions">
-              <Button color="accent">Transactions</Button>
-            </Link>
-            {/* <Link to="/account">
-              <Button color="contrast">Account</Button>
-            </Link> */}
-          </nav>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mobileNavAnchor: null
+    };
+
+    this.handleMobileNavOpen = this.handleMobileNavOpen.bind(this);
+    this.handleMobileNavClose = this.handleMobileNavClose.bind(this);
+  }
+
+  handleMobileNavOpen(event) {
+    this.setState({ mobileNavAnchor: event.currentTarget });
+  }
+
+  handleMobileNavClose(event) {
+    this.setState({ mobileNavAnchor: null });
+  }
+
+  render() {
+    const links = [
+      { location: '/', name: 'Overview'},
+      { location: '/blocks', name: 'Blocks'},
+      { location: '/transactions', name: 'Transactions'},
+      { location: '/accounts', name: 'Accounts'}
+    ]
+    return (
+      <div className="header">
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography type="headline" className="title">
+              Etheralyze
+            </Typography>
+            <nav className="desktop-nav">
+              {
+                links.map((link, idx) => (
+                  <Link key={idx} to={link.location}>
+                    <Button color="accent">{link.name}</Button>
+                  </Link>
+                ))
+              }
+            </nav>
+            <div className="mobile-nav">
+              <IconButton
+                onClick={this.handleMobileNavOpen}
+              >
+                <MoreVertIcon />
+              </IconButton>
+              <Menu
+                anchorEl={this.state.mobileNavAnchor}
+                open={!!this.state.mobileNavAnchor}
+                onRequestClose={this.handleMobileNavClose}
+                PaperProps={{
+                  style: {
+                    width: 200
+                  }
+                }}
+              >
+                {
+                  links.map((link, idx) => (
+                    <MenuItem
+                      id="mobile-nav__dropdownitem"
+                      key={idx}
+                      onClick={this.handleMobileNavClose}
+                    >
+                      <Link to={link.location}>
+                        {link.name}
+                      </Link>
+                    </MenuItem>
+                  ))
+                }
+              </Menu>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
 
 export default Header;
